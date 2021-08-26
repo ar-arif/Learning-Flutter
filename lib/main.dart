2f712971ba6bf7f96129603dc893473d4eda54bc
ar-arif <just.ar.arif@gmail.com>
2021-08-26 11:24:24 +0600

    ch#2 part-1 done

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,38 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
             title: Text("App Title"),
           ),
-        body: Container(
-            child: Center(child: Text("Hi Flutter !")),
+        body: Center(
+            child: Container(
+                padding: const EdgeInsets.all(8),
+                alignment: Alignment.center,
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color : Colors.grey,
+                        blurRadius: 5,
+                        offset: Offset(2.0, 5.0)
+                        )
+                    ],
+                    color: Colors.teal,
+                    gradient: LinearGradient(
+                        colors: [Colors.green, Colors.orange],
+                      ),
+                  ),
+                child: Text("I'm a Box",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20, 
+                          ),
+                  ),
+              ),
           ),
       );
   }
 }
+
 
